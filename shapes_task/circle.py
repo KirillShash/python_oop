73f034e5dab85dc717410d6416c6a0b61f983fcf
Kirill <dyky.ks@gmail.com>
2024-02-04 12:13:26 +0700

    first commit for shape task

---
+++ shapes_task/circle.py
@@ -0,0 +1,31 @@
+import math
+
+from shape import Shape
+
+
+class Circle(Shape):
+    def __init__(self, radius: float):
+        self.__radius = radius
+
+    @property
+    def radius(self):
+        return self.__radius
+
+    @radius.setter
+    def radius(self, radius: float):
+        self.__radius = radius
+
+    def get_width(self) -> float:
+        return self.__radius * 2
+
+    def get_height(self) -> float:
+        return self.__radius * 2
+
+    def get_area(self):
+        return math.pi * math.pow(self.__radius, 2)
+
+    def get_perimeter(self):
+        return 2 * math.pi * self.__radius
+
+    def __repr__(self):
+        return f'circle with radius = {self.__radius}'
---
+++ shapes_task/rectangle.py
@@ -0,0 +1,15 @@
+from shape import Shape
+
+
+class Rectangle(Shape):
+    def get_width(self):
+        pass
+
+    def get_height(self):
+        pass
+
+    def get_area(self):
+        pass
+
+    def get_perimeter(self):
+        pass

697a3da9c5d2aa9882612f69ac9882a4b8b4fd92
Kirill <dyky.ks@gmail.com>
2024-02-15 12:02:27 +0700

    add type hints in shape.py

--- shapes_task/shape.py
+++ shapes_task/shape.py
@@ -3,17 +3,17 @@ from abc import ABC, abstractmethod
 
 class Shape(ABC):
     @abstractmethod
-    def get_width(self):
+    def get_width(self) -> float:
         pass
 
     @abstractmethod
-    def get_height(self):
+    def get_height(self) -> float:
         pass
 
     @abstractmethod
-    def get_area(self):
+    def get_area(self) -> float:
         pass
 
     @abstractmethod
-    def get_perimeter(self):
+    def get_perimeter(self) -> float:
         pass

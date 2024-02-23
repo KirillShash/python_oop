7342a8a667fd9db2eed5317ad3b56f6911b559c0
Kirill <dyky.ks@gmail.com>
2024-02-23 19:58:21 +0700

    override __repr__

--- matrix_task/matrix.py
+++ matrix_task/matrix.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 from vector_task.vector import Vector
 from multipledispatch import dispatch
+from typing import override
 
 
 class Matrix:
-    @dispatch(Vector)
+    @dispatch(int, int)
     def __init__(self, rows_count: int, columns_count: int):
         if rows_count <= 0:
             raise ValueError(f'Rows count = {rows_count}, but the number of rows must be greater than 0.')
@@ -18,7 +19,7 @@ class Matrix:
     def __init__(self, matrix: Matrix):
         self.__vectors = list(matrix.__vectors)
 
-    @dispatch(list[list[float]])
+    @dispatch(list)
     def __init__(self, elements: list[list[float]]):
         if len(elements) == 0:
             raise ValueError(f'List length = {len(elements)} (row count), but number of rows must be greater than 0.')
@@ -35,7 +36,7 @@ class Matrix:
 
         self.__vectors = [Vector(max_columns_count, row) for row in elements]
 
-    @dispatch(list[Vector])
+    @dispatch(list)
     def __init__(self, vectors: list[Vector]):
         if len(vectors) == 0:
             raise ValueError(f'List is empty.')
@@ -47,3 +48,12 @@ class Matrix:
                 max_columns_count = row.dimension
 
         self.__vectors = [Vector(max_columns_count).add(vector) for vector in vectors]
+
+    @override
+    def __repr__(self) -> str:
+        result = '{'
+
+        for vector in self.__vectors:
+            result += str(vector) + ', '
+
+        return result[:-2] + '}'

4b1c8a4f8e0a60e104e660677924ab6c3aa36dbe
Kirill <dyky.ks@gmail.com>
2024-03-02 01:10:37 +0700

    rework constructors

--- matrix_task/matrix.py
+++ matrix_task/matrix.py
@@ -1,11 +1,54 @@
 from __future__ import annotations
 from vector_task.vector import Vector
-from multipledispatch import dispatch
-from typing import override
+from multimethod import multimethod
+from typing import override, List
 
 
 class Matrix:
-    @dispatch(int, int)
+    """
+        You can use constructors:
+            1. Matrix(rows_count: int, columns_count: int) – matrix of zeros of size rows_count x columns_count
+            2. Matrix(matrix: Matrix) – copy constructor
+            3. Matrix(elements: list[list[float]]) – from a two-dimensional list of numbers
+            4. Matrix(vectors: list[Vector]) – from a list of row vectors
+    """
+
+    @multimethod
+    def __init__(self, matrix: Matrix):
+        self.__vectors = list(matrix.__vectors)
+
+    @multimethod
+    def __init__(self, vectors: List[Vector]):
+        if len(vectors) == 0:
+            raise ValueError(f'List (vectors) is empty.')
+
+        max_columns_count = vectors[0].dimension
+
+        for row in vectors[1:]:
+            if row.dimension > max_columns_count:
+                max_columns_count = row.dimension
+
+        self.__vectors = [Vector(max_columns_count) + vector for vector in vectors]
+
+    @multimethod
+    def __init__(self, elements: List[List[float | int]]):
+        if len(elements) == 0:
+            raise ValueError(
+                f'List length = {len(elements)} (row count), but number of rows must be greater than 0.')
+
+        max_columns_count = 0
+
+        for row in elements:
+            if len(row) > max_columns_count:
+                max_columns_count = len(row)
+
+        if max_columns_count == 0:
+            raise ValueError(f'Nested array length = {max_columns_count} (column count), but number of columns must '
+                             f'be greater than 0.')
+
+        self.__vectors = [Vector(max_columns_count, row) for row in elements]
+
+    @multimethod
     def __init__(self, rows_count: int, columns_count: int):
         if rows_count <= 0:
             raise ValueError(f'Rows count = {rows_count}, but the number of rows must be greater than 0.')
@@ -15,45 +58,6 @@ class Matrix:
 
         self.__vectors = [Vector(columns_count) for _ in range(rows_count)]
 
-    @dispatch(object)
-    def __init__(self, matrix: Matrix):
-        self.__vectors = list(matrix.__vectors)
-
-    @dispatch(list)
-    def __init__(self, elements: list[list[float]]):
-        if len(elements) == 0:
-            raise ValueError(f'List length = {len(elements)} (row count), but number of rows must be greater than 0.')
-
-        max_columns_count = 0
-
-        for row in elements:
-            if len(row) > max_columns_count:
-                max_columns_count = len(row)
-
-        if max_columns_count == 0:
-            raise ValueError(f'Nested array length = {max_columns_count} (column count), but number of columns must be '
-                             f'greater than 0.')
-
-        self.__vectors = [Vector(max_columns_count, row) for row in elements]
-
-    @dispatch(list)
-    def __init__(self, vectors: list[Vector]):
-        if len(vectors) == 0:
-            raise ValueError(f'List is empty.')
-
-        max_columns_count = vectors[0].dimension
-
-        for row in vectors[1:]:
-            if row.dimension > max_columns_count:
-                max_columns_count = row.dimension
-
-        self.__vectors = [Vector(max_columns_count).add(vector) for vector in vectors]
-
     @override
     def __repr__(self) -> str:
-        result = '{'
-
-        for vector in self.__vectors:
-            result += str(vector) + ', '
-
-        return result[:-2] + '}'
+        return '{' + ', '.join(map(str, self.__vectors)) + '}'
---
+++ matrix_task/matrix_test.py
@@ -0,0 +1,14 @@
+from matrix import Matrix
+from vector_task.vector import Vector
+
+matrix_1 = Matrix(2, 2)
+matrix_2 = Matrix([[1.1, 2.2, 3.3], [4.4, 5.5, 6.6], [7.7, 8.8, 9.9]])
+matrix_3 = Matrix(matrix_2)
+
+vector = Vector(3, [1.1, 2.2, 3.3])
+matrix_4 = Matrix([vector, vector, vector])
+
+print(f'matrix_1: {matrix_1}')
+print(f'matrix_2: {matrix_2}')
+print(f'matrix_3: {matrix_3}')
+print(f'matrix_4: {matrix_4}')
